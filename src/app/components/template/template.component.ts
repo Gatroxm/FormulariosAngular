aa4641af210f5fb9358981c9dd7f635e6477a11e
Gustavo A. Muñoz <gustavoa.munoz@Gustavo.local>
2019-04-01 14:01:39 -0500

    se trabaja en obener los datos del formulario y se crean los componentes necesarios.

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent implements OnInit {
+
+  usuario: object = {
+    nombre: 'Gustavo',
+    apellido: 'Muñoz',
+    correo: 'tavoxpau@gmail.com'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log('Forma: ' , forma);
+    console.log('Valor: ' , forma.value);
+    console.log('Usuario: ' , this.usuario);
+  }
+}
