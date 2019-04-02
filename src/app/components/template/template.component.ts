1a8bc02580aed39de85b198b7b7eadd97f1bbfe7
Gatroxm <tavoxpau@gmail.com>
2019-04-02 06:55:46 -0500

    Validaciones en los formularios

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,14 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
 
   public usuario: object = {
-    nombre: 'Gustavo',
-    apellido: 'Muñoz',
-    correo: 'tavoxpau@gmail.com'
+    nombre: null,
+    apellido: null,
+    correo: null
   };
   constructor() { }
 
@@ -21,6 +25,23 @@ export class TemplateComponent implements OnInit {
   guardar(forma: NgForm) {
     console.log('Forma: ' , forma);
     console.log('Valor: ' , forma.value);
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
     // console.log('Usuario: ' , this.usuario);
   }
 }
